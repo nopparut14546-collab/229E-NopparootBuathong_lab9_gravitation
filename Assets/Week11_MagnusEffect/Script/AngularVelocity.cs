887be8dfc7ea5510a1d71e1ae2fb45f05da11228
nopparut14546-collab <nopparut.14546@gmail.com>
2026-03-26 16:16:19 +0700

    Lab10_MagnusEffect

--- Assets/Week11_MagnusEffect/Script/AngularVelocity.cs
+++ Assets/Week11_MagnusEffect/Script/AngularVelocity.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AngularVelocity : MonoBehaviour
 {
+    public float angularSpeed = 1f;
+    private Rigidbody _rb;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Keyboard.current.aKey.isPressed)
+        {
+            _rb.angularVelocity = new Vector3(0, angularSpeed, 0); //เปลี่ยนความเร็วจากตัวเเปร
+        }
+        else
+        {
+            _rb.angularVelocity = Vector3.zero;
+        }
     }
 }
---
+++ Assets/Week11_MagnusEffect/Script/TorqueControl.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TorqueControl : MonoBehaviour
+{
+    public float torquePower = 1.0f;
+    private Rigidbody _rb;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Keyboard.current.dKey.isPressed)
+        {
+            _rb.AddRelativeTorque(0,0, torquePower); 
+        }
+    }
+}
